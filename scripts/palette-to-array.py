a0259b0b3d8d170bc728213b32fcfc8def6f0565
Vulcalien <vulcalien@gmail.com>
2023-07-15 12:51:37 +0200

    palette-to-array.py: use 'argparse' to improve command line interface
    Also, the script now supports the '-o' option, used to specify the
    output file. The default is stdout.

--- scripts/palette-to-array.py
+++ scripts/palette-to-array.py
@@ -4,21 +4,35 @@
 # The output is a C array of type 'const u16' and length 16.
 #
 # Usage: palette-to-array.py <palette_image_file> <array_name>
-# The output is printed to stdout.
 
-from sys import argv, exit
+import sys, argparse
 from PIL import Image
 
-if len(argv) < 3:
-    print('Usage: ' + argv[0] + ' <palette_image_file> <array_name>')
-    exit()
+# Setup argparse
+parser = argparse.ArgumentParser(
+    description='Generate a GBA palette from a 16-pixel image'
+)
 
-img = Image.open(argv[1]).convert('RGB')
+parser.add_argument('palette_filename',
+                    type=argparse.FileType('rb'),
+                    help='specify the filename of the palette image')
+parser.add_argument('array_name',
+                    help='specify the name of the output array')
+
+parser.add_argument('-o', '--output',
+                    type=argparse.FileType('w'), default=sys.stdout,
+                    help='specify the output file (default: stdout)')
+
+args = parser.parse_args()
+
+# Open and validate image
+img = Image.open(args.palette_filename).convert('RGB')
 
 if img.width * img.height != 16:
     print('Error: the palette image file must contain exactly 16 pixels')
     exit()
 
+# Get and convert colors
 colors = []
 for i in range(16):
     pix = img.getpixel( (i % img.width, i // img.width) )
@@ -33,11 +47,13 @@ for i in range(16):
     colors.append(color_code)
 
 # Print output
-print('const u16 ' + argv[2] + '[16] = {', end='')
+f = args.output
+
+f.write('const u16 ' + args.array_name + '[16] = {')
 for i in range(16):
     if i % 8 == 0:
-        print('\n    ', end='')
-    print(colors[i], end='')
+        f.write('\n    ')
+    f.write(colors[i])
     if i != 15:
-        print(', ', end='')
-print('\n};')
+        f.write(', ')
+f.write('\n};\n')
