eb9fa358cb700cfe3bd25f1dd5127f410bc186fa
Vulcalien <vulcalien@vulcalien.net>
2024-02-03 14:28:31 +0100

    Add 'ALWAYS_INLINE' attribute to 'itoa' in 'util.h'
    Now that ALWAYS_INLINE is defined before 'util.h' is included, the
    'itoa' function can use that attribute.

--- include/util.h
+++ include/util.h
@@ -23,7 +23,9 @@ extern void memcpy16(vu16 *dest, const vu16 *src, u32 n);
 extern u16 rand(void);
 extern void srand(u32 val, bool reset);
 
-inline void itoa(u32 number, u8 radix, char *array, u8 digits, bool zero_fill) {
+ALWAYS_INLINE
+inline void itoa(u32 number, u8 radix, char *array,
+                 u8 digits, bool zero_fill) {
     u32 pos = 0;
     for(u32 i = 0; i < digits; i++) {
         u32 digit = number;
