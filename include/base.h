fda7689669725ee1f9d6a7a86b2dbf383d5fe4a8
Vulcalien <vulcalien@vulcalien.net>
2024-04-06 16:24:20 +0200

    Merge 'types.h' into 'base.h'

--- include/base.h
+++ include/base.h
@@ -17,7 +17,39 @@
 
 // Basic types and definitions used throughout the program.
 
-#include "types.h"
+// === Data Types ===
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+
+typedef volatile uint8_t  vu8;
+typedef volatile uint16_t vu16;
+typedef volatile uint32_t vu32;
+typedef volatile int8_t   vi8;
+typedef volatile int16_t  vi16;
+typedef volatile int32_t  vi32;
+
+// Limits
+#define  U8_MAX (255)
+#define U16_MAX (65535)
+#define U32_MAX (4294967295)
+
+#define  I8_MIN (-128)
+#define I16_MIN (-32768)
+#define I32_MIN (-2147483648)
+
+#define  I8_MAX (127)
+#define I16_MAX (32767)
+#define I32_MAX (2147483647)
+
+// ===== ===== =====
 
 #ifndef NULL
     #define NULL ((void *) 0)
